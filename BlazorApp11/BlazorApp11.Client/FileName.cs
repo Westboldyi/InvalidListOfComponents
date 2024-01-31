0658aac6ad6194ed8575d0982082f1af13cc946f
YZ <yi@westbold>
2024-01-31 12:17:53 -0800

    Add project files.

---
+++ BlazorApp11/BlazorApp11.Client/FileName.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorApp11.Client
+{
+    public class InitialApplicationStateProvider
+    {
+        public InitialApplicationState? AppState { get; set; }
+    }
+
+    public class InitialApplicationState
+    {
+        public required string? ValueA { get; set; }
+        public required string? ValueB { get; set; }
+        public required string? ValueC { get; set; }
+    }
+}
---
+++ BlazorApp11/BlazorApp11/Middleware.cs
@@ -0,0 +1,27 @@
+﻿using BlazorApp11.Client;
+
+public class AppStateMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public AppStateMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var appState = new InitialApplicationState()
+        {
+            ValueA = "aaa",
+            ValueB = null,
+            ValueC = null
+
+        };
+        var initialAppStateProvider = context.RequestServices.GetRequiredService<InitialApplicationStateProvider>();
+        initialAppStateProvider.AppState = appState;
+
+        await _next(context);
+    }
+}
+
